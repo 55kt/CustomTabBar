6ec419a3db4875d380f1f0806695eea31513c763
Vladyslav R <160039489+55kt@users.noreply.github.com>
2025-05-11 20:04:30 +0300

    Initial Commit

---
+++ CustomTabBar/CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarApp.swift
+//  CustomTabBar
+//
+//  Created by Vlad on 11/5/25.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
